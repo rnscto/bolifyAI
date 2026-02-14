b55f672c3e368a1a3d37a252ec1f87619fae7cf7
Neeraj Yadav <neerajyrns@gmail.com>
2026-02-14 12:42:17 +0000

    File changes

--- functions/streamAudio.ts
+++ functions/streamAudio.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest, createClient } from 'npm:@base44/sdk@0.8.18';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.18';
 
 const STATE = {
   IDLE: 'IDLE',
