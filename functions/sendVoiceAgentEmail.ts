c203e0a1fab69326d91188ed18da335c90db078c
Neeraj Yadav <neerajyrns@gmail.com>
2026-02-19 05:35:02 +0000

    File changes

--- functions/sendVoiceAgentEmail.ts
+++ functions/sendVoiceAgentEmail.ts
@@ -132,7 +132,7 @@ Deno.serve(async (req) => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        from: 'VaaniAI <sales@sales.vaaniai.io>',
+        from: 'VaaniAI <sales@vaaniai.io>',
         to: email,
         subject: template.subject,
         html: finalBody
