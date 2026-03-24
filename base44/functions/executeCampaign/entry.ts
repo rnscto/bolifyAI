311adc299958d50a8d1b124a5d33f0186a8db0f1
Neeraj Yadav <neerajyrns@gmail.com>
2026-03-24 06:04:13 +0000

    File changes

--- base44/functions/executeCampaign/entry.ts
+++ base44/functions/executeCampaign/entry.ts
@@ -278,7 +278,9 @@ Deno.serve(async (req) => {
             agent_name: agent.name, system_prompt: personalizedPrompt,
             persona: agent.persona || {}, knowledge_base_content: kbContent,
             lead_context: leadContext,
-            greeting_message: agent.greeting_message || ''
+            greeting_message: agent.greeting_message || '',
+            human_transfer_number: agent.human_transfer_number || '',
+            enable_auto_transfer: agent.enable_auto_transfer !== false
           }
         });
 
