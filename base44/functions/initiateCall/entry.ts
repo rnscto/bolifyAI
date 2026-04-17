7f433f6d825ceaa89b1454cb670b311aa6d1a72f
Neeraj Yadav <neerajyrns@gmail.com>
2026-04-17 11:15:34 +0000

    File changes

--- base44/functions/initiateCall/entry.ts
+++ base44/functions/initiateCall/entry.ts
@@ -1,5 +1,6 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.25';
 
+// Initiate outbound call via Smartflo telephony
 Deno.serve(async (req) => {
   try {
     // Support three auth methods:
