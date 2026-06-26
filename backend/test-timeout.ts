558e00b7df15103e17616436940439d927ddae15
nandyadav <yadav.nandkishor73@gmail.com>
2026-06-26 21:01:04 +0530

    chore: remove test script

--- backend/test-timeout.ts
+++
@@ -1,6 +0,0 @@
-const timeout = (ms: number) => new Promise((_, reject) => setTimeout(() => reject(new Error("Timeout")), ms));
-const result = await Promise.race([
-  new Promise(resolve => setTimeout(() => resolve("Success"), 2000)),
-  timeout(1000)
-]).catch(e => e.message);
-console.log(result);
