e915ea49dae77f9b5fc05db604157163181abfb8
nandyadav <yadav.nandkishor73@gmail.com>
2026-06-26 20:52:14 +0530

    chore: remove test API scripts

--- test-api.js
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch";
-
-async function run() {
-  const res = await fetch("http://localhost:8000/api/reseller/custom-domain-config");
-  const text = await res.text();
-  console.log("Status:", res.status);
-  console.log("Body:", text);
-}
-run();
